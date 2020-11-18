9c1f86d8050a64d6807197c0b4de7a68bd30d0db
keiyanishio <keiyan@al.insper.edu.br>
2020-11-18 16:45:53 -0300

    Adicionado estados de jogo Implementada classe incial de vetor 2d

--- pong.py
+++ pong.py
@@ -1,5 +1,9 @@
 # init / dependencias
 import pygame
+import math
+import random
+import time
+from copy import copy
 
 # init / janela
 WIDTH = 1280
@@ -9,31 +13,69 @@ pygame.display.set_caption('Pong')
 
 # init / parametros de jogabilidade
 clock = pygame.time.Clock()
+# taxa de frames por segundo
 framerate = 60
 
-active_game = True
+# init / estados de jogo
+STATE_MENU = 1 << 0
+STATE_PLAYING = 1 << 1
+STATE_ROUND_START = 1 << 2
+STATE_QUIT = 1 << 3
+game_state = STATE_MENU
 
 pygame.init()
 
+# classe de vetores 2d
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 def frame_think():
-    global active_game
+    global game_state
+
     for event in pygame.event.get():
-    # jogo / gerenciador de eventos
+    # jogo / buffer de eventos
         if event.type == pygame.QUIT:
+            # caso o cliente tente fechar o aplicativo, executar a sequencia abaixo
             print("pygame.QUIT")
-            active_game = False
+            game_state = STATE_QUIT
             break
 
 def frame_render():
-    window.fill( (0,0,0) )
-    pygame.display.update()
+    return
 
-while active_game:
+while game_state != STATE_QUIT:
     # jogo / definir framerate
     clock.tick(framerate)
+    #print("framerate: {0} | tempo desde o último frame: {1}ms (estável em {2}ms)".format(int(1/(frametime/1000)), frametime/1000.0, 1.0/framerate))
 
-    frame_think()
-    frame_render()
+    # background preto
+    window.fill( (0,0,0) )
+
+    # jogo / diferenciação de estados
+    if game_state == STATE_MENU:
+        for event in pygame.event.get():
+        # jogo / buffer de eventos
+            if event.type == pygame.QUIT:
+                # caso o cliente tente fechar o aplicativo, executar a sequencia abaixo
+                print("pygame.QUIT")
+                game_state = STATE_QUIT
+                break
+
+    elif game_state == STATE_PLAYING or game_state == STATE_ROUND_START:
+        # execução de estágios do frame
+        if game_state == STATE_ROUND_START:
+            # alterações antes do round recomeçar + tempo de pausa antes da bola se mover
+            time.sleep(1)
+            game_state = STATE_PLAYING
+
+
+        frame_think()
+        frame_render()
+    else:
+        pass
+
+    pygame.display.update()
 
 pygame.quit()
-        
